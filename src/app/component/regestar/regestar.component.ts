866e8d536132f240507fe26ac0cd31f6fd8c636d
M0hamedA7med <161088706+M0hamedA7med@users.noreply.github.com>
2024-03-13 22:49:20 +0200

    login&regiestar

---
+++ src/app/component/regestar/regestar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regestar',
+  standalone: true,
+  imports: [],
+  templateUrl: './regestar.component.html',
+  styleUrl: './regestar.component.css'
+})
+export class RegestarComponent {
+
+}
