3680bbace09562217d5e8f5caa8a014c21565363
ahmeddddddddddddddddddd <ahmedhossam.ah1221@gmail.com>
2024-03-14 23:10:28 +0200

    new

--- src/app/component/DashBoard/dash-board-grade-one/dash-board-grade-one.component.ts
+++ src/app/component/DashBoard/dash-board-grade-one/dash-board-grade-one.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dash-board-grade-one',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './dash-board-grade-one.component.html',
   styleUrl: './dash-board-grade-one.component.css'
 })
 export class DashBoardGradeOneComponent {
+  selectedSubject: string | null = null;
+  subjects = ['Math', 'Arabic', 'Science', 'English', 'Social Studies'];
 
+  submitForm(form: NgForm) {
+    // Handle form submission logic here
+  }
 }
