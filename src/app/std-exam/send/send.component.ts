9b7194a17a9fd0b965047708e120e09edd89b9fc
ibrahem refeay <137005249+ibrahemrefai2023@users.noreply.github.com>
2024-03-22 02:09:35 +0200

    add

--- src/app/std-exam/send/send.component.ts
+++ src/app/std-exam/send/send.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-send',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './send.component.html',
   styleUrl: './send.component.css'
 })
