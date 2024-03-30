aede0a9aa64ea0e6418249bc76de2110e92759e8
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-30 22:12:50 +0200

    Edite_Some_Points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,42 +2,19 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { RouterOutlet } from "@angular/router";
-<<<<<<< Updated upstream
+
 import { choosegradeComponent } from "./Student/grades/ChooseGrade/ChooseGrade.component";
-import { ChooseinstructorComponent } from "./Student/grades/ChooseGroup/ChooseGroup.component";
 import { CourseselectedComponent } from "./Student/courseselected/courseselected/courseselected.component";
 import { ExamPageComponent } from "./Student/showExam/std-exam/exam-page/exam-page.component";
-=======
 import { MainComponent } from "./Student/courseselected/main/main.component";
 import { CoursesComponent } from "./Student/courseselected/courses/courses.component";
 
->>>>>>> Stashed changes
-
-
-
-
-
 @Component({
-<<<<<<< Updated upstream
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    HeaderComponent,FooterComponent,RouterOutlet,choosegradeComponent,CourseselectedComponent,ExamPageComponent
-  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-=======
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [
-        HeaderComponent, FooterComponent, RouterOutlet,
-        MainComponent,CoursesComponent
-    ]
->>>>>>> Stashed changes
+  styleUrls: ['./app.component.css'],
+  providers: [HeaderComponent, FooterComponent, RouterOutlet, choosegradeComponent, CourseselectedComponent, ExamPageComponent, MainComponent, CoursesComponent]
 })
 export class AppComponent {
   title: any;
-
 }
