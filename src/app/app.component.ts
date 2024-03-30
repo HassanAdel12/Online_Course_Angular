010d855175d64db9f5509e8fc6a41e798c18b08b
ibrahem refeay <137005249+ibrahemrefai2023@users.noreply.github.com>
2024-03-30 05:39:23 +0200

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { RouterOutlet } from "@angular/router";
+import { choosegradeComponent } from "./Student/grades/ChooseGrade/ChooseGrade.component";
+import { ChooseinstructorComponent } from "./Student/grades/ChooseGroup/ChooseGroup.component";
+import { CourseselectedComponent } from "./Student/courseselected/courseselected/courseselected.component";
+import { ExamPageComponent } from "./Student/showExam/std-exam/exam-page/exam-page.component";
 
 
 
@@ -11,7 +15,7 @@ import { RouterOutlet } from "@angular/router";
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent,FooterComponent,RouterOutlet
+    HeaderComponent,FooterComponent,RouterOutlet,choosegradeComponent,CourseselectedComponent,ExamPageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
